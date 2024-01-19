38f7bc92c6b0565919b91f0e405b91a29403faed
Jimmy-7664 <47916283+Jimmy-7664@users.noreply.github.com>
2024-01-19 12:52:45 +0900

    Revert "Fix device initing in multi-node training"

--- stdmae/stdmae_arch/mask/positional_encoding.py
+++ stdmae/stdmae_arch/mask/positional_encoding.py
@@ -22,6 +22,6 @@ class PositionalEncoding(nn.Module):
         """
 
         batch_size, num_nodes, num_patches, num_feat = input_data.shape
-        tp_enc_2d = PositionalEncoding2D(num_feat).to(input_data.device)
+        tp_enc_2d = PositionalEncoding2D(num_feat)
         input_data+=tp_enc_2d(input_data)
         return input_data,tp_enc_2d(input_data)
