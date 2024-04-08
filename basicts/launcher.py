663d895d3d493131945c5a81ceb7b29ab98fae97
Haotian Gao <47916283+Jimmy-7664@users.noreply.github.com>
2024-04-08 23:20:53 +0800

    Update launcher.py

--- basicts/launcher.py
+++ basicts/launcher.py
@@ -1,5 +1,4 @@
 from typing import Dict, Union
-from packaging import version
 
 import easytorch
 
@@ -17,8 +16,12 @@ def launch_training(cfg: Union[Dict, str], gpus: str = None, node_rank: int = 0)
     # config checking
     pass
     # launch training based on easytorch
-    easytorch_version = easytorch.__version__
-    if version.parse(easytorch_version) >= version.parse("1.3"):
+    try:
         easytorch.launch_training(cfg=cfg, devices=gpus, node_rank=node_rank)
-    else:
-        easytorch.launch_training(cfg=cfg, gpus=gpus, node_rank=node_rank)
+    except TypeError as e:
+        if "launch_training() got an unexpected keyword argument" in repr(e):
+            # NOTE: for earlier easytorch version
+            easytorch.launch_training(cfg=cfg, gpus=gpus, node_rank=node_rank)
+        else:
+            raise e
+
