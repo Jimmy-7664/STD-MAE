d434a6a6c13d71cc11e93170520c8c769a4e73b9
Jimmy-7664 <47916283+Jimmy-7664@users.noreply.github.com>
2024-12-06 00:43:20 +0900

    Update LA&BAY

--- scripts/data_preparation/all.sh
+++ scripts/data_preparation/all.sh
@@ -3,8 +3,17 @@ python scripts/data_preparation/PEMS03/generate_training_data.py --history_seq_l
 python scripts/data_preparation/PEMS04/generate_training_data.py --history_seq_len 12
 python scripts/data_preparation/PEMS07/generate_training_data.py --history_seq_len 12
 python scripts/data_preparation/PEMS08/generate_training_data.py --history_seq_len 12
+python scripts/data_preparation/PEMS-BAY/generate_training_data.py --history_seq_len 12
+python scripts/data_preparation/METR-LA/generate_training_data.py --history_seq_len 12
+python scripts/data_preparation/PEMSD7M/generate_training_data.py --history_seq_len 12
+python scripts/data_preparation/PEMSD7L/generate_training_data.py --history_seq_len 12
+
 
 python scripts/data_preparation/PEMS03/generate_training_data.py --history_seq_len 864
 python scripts/data_preparation/PEMS04/generate_training_data.py --history_seq_len 864
 python scripts/data_preparation/PEMS07/generate_training_data.py --history_seq_len 864
 python scripts/data_preparation/PEMS08/generate_training_data.py --history_seq_len 2016
+python scripts/data_preparation/PEMS-BAY/generate_training_data.py --history_seq_len 864
+python scripts/data_preparation/METR-LA/generate_training_data.py --history_seq_len 864
+python scripts/data_preparation/PEMSD7M/generate_training_data.py --history_seq_len 2016
+python scripts/data_preparation/PEMSD7L/generate_training_data.py --history_seq_len 864
