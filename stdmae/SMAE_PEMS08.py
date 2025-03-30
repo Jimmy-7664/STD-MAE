f299aa96c207bd7a9c9d79a28b8fbfc5bf76c12f
Haotian Gao <jimmygao1202@gmail.com>
2025-03-30 17:09:15 +0800

    Update SMAE_PEMS08.py

--- stdmae/SMAE_PEMS08.py
+++ stdmae/SMAE_PEMS08.py
@@ -25,7 +25,7 @@ BATCH_SIZE_ALL=4
 
 # ================= environment ================= #
 CFG.ENV = EasyDict()
-CFG.ENV.SEED = random.randint(0,10000000)
+CFG.ENV.SEED = 0
 CFG.ENV.CUDNN = EasyDict()
 CFG.ENV.CUDNN.ENABLED = True
 
