58aa00b8248481a2b1d676677f3cff126c81211a
Haotian Gao <jimmygao1202@gmail.com>
2025-04-07 12:47:52 +0900

    Update stdmae.py

--- stdmae/stdmae_arch/stdmae.py
+++ stdmae/stdmae_arch/stdmae.py
@@ -2,11 +2,11 @@ import torch
 from torch import nn
 
 from .mask import Mask
-from .graphwavenet import GraphWaveNet
+from .dcrnn import DCRNN
 
 
 class STDMAE(nn.Module):
-    """Spatio-Temporal-Decoupled Masked Pre-training for Traffic Forecasting"""
+    """Pre-training Enhanced Spatial-temporal Graph Neural Network for Multivariate Time Series Forecasting"""
 
     def __init__(self, dataset_name, pre_trained_tmae_path,pre_trained_smae_path, mask_args, backend_args):
         super().__init__()
@@ -17,7 +17,7 @@ class STDMAE(nn.Module):
         self.tmae = Mask(**mask_args)
         self.smae = Mask(**mask_args)
 
-        self.backend = GraphWaveNet(**backend_args)
+        self.backend = DCRNN(**backend_args)
 
         # load pre-trained model
         self.load_pre_trained_model()
@@ -39,29 +39,26 @@ class STDMAE(nn.Module):
         for param in self.smae.parameters():
             param.requires_grad = False
     def forward(self, history_data: torch.Tensor, long_history_data: torch.Tensor, future_data: torch.Tensor, batch_seen: int, epoch: int, **kwargs) -> torch.Tensor:
-        """Feed forward of STDMAE.
-
-        Args:
-            history_data (torch.Tensor): Short-term historical data. shape: [B, L, N, 3]
-            long_history_data (torch.Tensor): Long-term historical data. shape: [B, L * P, N, 3]
-
-        Returns:
-            torch.Tensor: prediction with shape [B, N, L].
-        """
 
         # reshape
         short_term_history = history_data     # [B, L, N, 1]
-
+        long_term_history = long_history_data
         batch_size, _, num_nodes, _ = history_data.shape
 
         hidden_states_t = self.tmae(long_history_data[..., [0]])
         hidden_states_s = self.smae(long_history_data[..., [0]])
         hidden_states=torch.cat((hidden_states_t,hidden_states_s),-1)
         
-        # enhance
+        # enhancing 
         out_len=1
+        # hidden_states = hidden_states[:, :, -out_len:, :]# B,N,T,C=>B,N,96
         hidden_states = hidden_states[:, :, -out_len, :]
-        y_hat = self.backend(short_term_history, hidden_states=hidden_states).transpose(1, 2).unsqueeze(-1)
 
+        y_hat = self.backend(history_data,future_data=future_data,batch_seen=batch_seen,hidden_states=hidden_states )
+        
+        # hidden_states=hidden_states.view(batch_size,num_nodes,-1,1).transpose(1,2)
+
+
+        #y_hat = self.backend(hidden_states)
         return y_hat
 
