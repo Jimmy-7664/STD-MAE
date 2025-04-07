213501bd3aaa7b3a109150e0716ac416a19e797e
Haotian Gao <jimmygao1202@gmail.com>
2025-04-07 12:29:26 +0900

    Update __init__.py

--- stdmae/stdmae_runner/__init__.py
+++ stdmae/stdmae_runner/__init__.py
@@ -1,5 +1,5 @@
 from .mask_runner import MaskRunner
 from .stdmae_runner import STDMAERunner
-from .stdmaedcrnn_runner import STDMAEDCRNNRunner
+from .stdmae_dcrnn_runner import STDMAEDCRNNRunner
 
 __all__ = ["MaskRunner", "STDMAERunner","STDMAEDCRNNRunner"]
